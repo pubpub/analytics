df3244e9d0b03681689c5969aa8a50f157be5835
Gabriel Stein <g@gabestein.com>
2019-07-30 13:51:19 -0400

    new: SASS loading and main styles.

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 
+import '../styles/base.scss';
+
 export default ({ children, title = 'PubPub Analytics' }) => (
 	<div>
 		<Head>
@@ -8,17 +10,19 @@ export default ({ children, title = 'PubPub Analytics' }) => (
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		</Head>
-		<header>
-			<h1>PubPub Analytics Dashboard</h1>
-		</header>
-		<div className="content">{children}</div>
-		<footer>
-			<a href="https://www.netlify.com">
-				<img
-					src="https://www.netlify.com/img/global/badges/netlify-light.svg"
-					alt="light netlify badge"
-				/>
-			</a>
-		</footer>
+		<div className="container wide">
+			<header>
+				<h1>PubPub Analytics Dashboard</h1>
+			</header>
+			<div className="content">{children}</div>
+			<footer>
+				<a href="https://www.netlify.com">
+					<img
+						src="https://www.netlify.com/img/global/badges/netlify-light.svg"
+						alt="light netlify badge"
+					/>
+				</a>
+			</footer>
+		</div>
 	</div>
 );
--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 require('dotenv').config({ path: '.env' });
 
-module.exports = withCSS({
-	/* config options here */
-	serverRuntimeConfig: {},
-	publicRuntimeConfig: {
-		KEEN_PROJECT_ID: process.env.KEEN_PROJECT_ID,
-		KEEN_READ_KEY: process.env.KEEN_READ_KEY,
-	},
-});
+module.exports = withSass(
+	withCSS({
+		/* config options here */
+		serverRuntimeConfig: {},
+		publicRuntimeConfig: {
+			KEEN_PROJECT_ID: process.env.KEEN_PROJECT_ID,
+			KEEN_READ_KEY: process.env.KEEN_READ_KEY,
+		},
+	}),
+);
